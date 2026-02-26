e9f1e8850677e9c850fab86618a1c5a2878261d4
Okoro Redemption <okororedemption24@gmail.com>
2026-02-26 07:56:22 +0100

    chore: try fixing better-auth issue

--- configs/auth.server.ts
+++ configs/auth.server.ts
@@ -2,5 +2,5 @@ import { defineServerAuth } from '@onmax/nuxt-better-auth/config'
 
 export default defineServerAuth({
   emailAndPassword: { enabled: true },
-  trustedOrigins: ['http://localhost:3000']
+  trustedOrigins: ['http://localhost:3000', "https://anatarenigeria.org"]
 })
