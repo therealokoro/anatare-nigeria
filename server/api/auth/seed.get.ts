06a61c331f51f9c9ed6c09092f1b4a7855705d3c
Okoro Redemption <okororedemption24@gmail.com>
2026-02-27 23:31:05 +0100

    updated seed api end point

--- server/api/auth/seed.get.ts
+++ server/api/auth/seed.get.ts
@@ -1,7 +1,31 @@
-export default eventHandler(async (event) => {
-  // IMPORTANT: Authenticate user and validate payload!
-  const payload = { ...getQuery(event) };
-  const { result } = await runTask("db:seed", { payload });
+import { eq } from "drizzle-orm"
+import { db } from "@nuxthub/db"
+import { user } from "#auth/schema"
 
-  return { result };
+
+export default eventHandler(async () => {  
+  console.log("Running DB seed task...");
+
+  const config = useRuntimeConfig()
+  const auth = serverAuth()
+
+  const [adminExist] = await db.select().from(user).where(eq(user.email, config.adminEmail))
+
+  if (adminExist) {
+    console.log("Admin already exists, skipping seed.")
+    return { result: "Skipped" }
+  }
+
+  console.log("Creating admin...")
+
+  await auth.api.signUpEmail({
+    body: {
+      name: "Administrator",
+      email: config.adminEmail,
+      password: config.adminPass
+    }
+  })
+
+  console.log("Admin created successfully.")
+  return { result: "Success" }  
 });
